68b4b64f97cff262b6f72b2d7303699a3d85e465
ebysofyan <eby.sofyan@gmail.com>
2017-07-05 17:44:41 +0800

    initial commit

---
+++ programming/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Programming
+
+# Register your models here.
+admin.site.register(Programming)
---
+++ programming/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Programming(models.Model):
+    nama = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.nama
